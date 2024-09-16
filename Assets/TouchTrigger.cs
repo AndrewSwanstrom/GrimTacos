48619807d1d4d0e2a30a651d2e94c5479be01b83
aryafire1 <erthorsen876@gmail.com>
2024-09-16 18:03:47 -0400

    dash added yayyyyy

--- Assets/TouchTrigger.cs
+++ Assets/TouchTrigger.cs
@@ -5,21 +5,39 @@ using UnityEngine;
 public class TouchTrigger : MonoBehaviour
 {
     public float jumpForce;
+    public float dashForce;
     public int jumpCount;
+    public float screenPercent;
+
     int isGrounded;
+    float dragDistance;
+    Vector3 firstTouch;
+    Vector3 lastTouch;
 
     Touch tap;
     Rigidbody rb;
+    Transform obj;
 
     void Start() {
         rb = GetComponent<Rigidbody>();
         jumpCount = jumpCount - 1;
+        dragDistance = Screen.height * screenPercent/100;
     }
 
     // Update is called once per frame
     void Update()
     {
         TapReader();
+    }
+
+    void OnCollisionStay(Collision collider) {
+        if (collider.gameObject.tag == "Ground") {
+        isGrounded = 0;
+        }
+        else if (collider.gameObject.tag == "Obstacle") {
+            obj = GetComponent<Transform>();
+            obj.position = Vector3.zero;
+        }
     }
 
     void TapReader() {
@@ -29,6 +47,8 @@ public class TouchTrigger : MonoBehaviour
             if (tap.phase == TouchPhase.Began && isGrounded < jumpCount) {
                 Debug.Log("Tap");
                 isGrounded++;
+                firstTouch = tap.position;
+                lastTouch = tap.position;
 
                 rb.velocity += jumpForce * Vector3.up;
 
@@ -36,11 +56,18 @@ public class TouchTrigger : MonoBehaviour
             else if (tap.phase == TouchPhase.Began && isGrounded >= jumpCount) {
                 Debug.Log("No more jump");
             }
+            else if (tap.phase == TouchPhase.Moved) {
+                lastTouch = tap.position;
+                Movement();
+            }
         }
     }
 
-    void OnCollisionStay() {
-        isGrounded = 0;
-        //Debug.Log("grounded"); <- working
+    void Movement() {
+        if (Mathf.Abs(lastTouch.x - firstTouch.x) > dragDistance || Mathf.Abs(lastTouch.y - firstTouch.y) > dragDistance) {
+                rb.velocity += dashForce * Vector3.right;
+        }
     }
+
+
 }
