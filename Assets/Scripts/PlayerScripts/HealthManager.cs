2ab467a4a573fc0eb4f111466e61db1c3a3fb4c0
XxSWxX <Tmdurbin2@yahoo.com>
2024-10-02 00:13:18 -0400

    HP UI added

--- Assets/Scripts/PlayerScripts/HealthManager.cs
+++ Assets/Scripts/PlayerScripts/HealthManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthManager : MonoBehaviour
 {
@@ -8,22 +9,65 @@ public class HealthManager : MonoBehaviour
     Transform obj;
     TapReader tapScript = new TapReader();
 
-    public int health;
+    // i dont understand this stuff i did but it gives health a variable changed event so its goated
+    public int maxHealth;
+    public int health = 3;
+    private int healthVar
+    {
+        get { return health; }
+        set
+        {
+            if (health == value) return;
+            if (health > value) { VariableChangeDown(health, value); } //if goes up
+            //if (health < value) {  } // if goes down
+            health = value;
+            
+        }
+    }
 
+    public GameObject[] Hearts;
 
-    void OnCollisionStay(Collision collider) {
+    private GameObject checkpoint;
+
+    private void VariableChangeDown(int var, int newvar)
+    {
+        Hearts[newvar].gameObject.GetComponent<Image>().color = new Color(1,1,1, 0f);
+        Hearts[newvar].transform.Find("BrokenImage").gameObject.SetActive(true);
+    }
+
+    void OnTriggerEnter(Collider collider) {
         if (collider.gameObject.tag == "Ground") {
         tapScript.isGrounded = 0;
         }
         else if (collider.gameObject.tag == "Obstacle") {
-            health --;
-            Debug.Log($"Health: {health}");
-            if (health == 0) {
-                transform.DetachChildren();
-                Destroy(this.gameObject);
+            healthVar--;
+
+            if (healthVar == 0) {
+                //transform.DetachChildren();
+                //Destroy(this.gameObject);
+                obj = GetComponent<Transform>();
+
+                checkpoint = gameObject.GetComponent<ActivateCheckpoint>().currentCheckpoint;
+                if (checkpoint != null)
+                {
+                    obj.position = checkpoint.transform.position;
+                }
+                else
+                {
+                    obj.position = Vector3.zero;
+                }
+
+                healthVar = maxHealth;
+                for (int i = 0; i < Hearts.Length; i++)
+                {
+                    Hearts[i].gameObject.GetComponent<Image>().color = new Color(1, 1, 1, 1f);
+                    Hearts[i].transform.Find("BrokenImage").gameObject.SetActive(false);
+                }
             }
-            obj = GetComponent<Transform>();
-            obj.position = Vector3.zero;
+
         }
     }
+
+
+
 }
