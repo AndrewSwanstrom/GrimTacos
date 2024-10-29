06e6ce4dbfa87993db5cb83b05b254a19878e737
aryafire1 <erthorsen876@gmail.com>
2024-10-29 17:09:02 -0400

    hold this for me before i mess around with something else

--- Assets/Scripts/WorldScripts/InfTrigger.cs
+++ Assets/Scripts/WorldScripts/InfTrigger.cs
@@ -6,16 +6,17 @@ public class InfTrigger : MonoBehaviour
 {
 
     public GameObject[] genChunks;
-    public float chunkWidth;
+    public float chunkWidth = 20f;
 
     Vector3 genPosition;
 
 
     void OnTriggerEnter (Collider collider) {
         if (collider.gameObject.CompareTag("Player")) {
-            /* old from reg player usage
+
             genPosition = collider.gameObject.transform.position;
-            genPosition = new Vector3(genPosition.x * 2, genPosition.y, genPosition.z);*/
+            genPosition = new Vector3(genPosition.x + chunkWidth, genPosition.y, genPosition.z);
+
             Instantiate(genChunks[Random.Range(0, genChunks.Length)], genPosition, Quaternion.identity);
         }
     }
