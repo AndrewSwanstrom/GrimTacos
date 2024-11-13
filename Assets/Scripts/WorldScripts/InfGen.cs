cb3c347ce2775a601a6393d2cd70f6357908709f
aryafire1 <erthorsen876@gmail.com>
2024-11-13 13:40:22 -0500

    infgen set up and working, needs adjusting with variables, list for optimization purposes isn't working

---
+++ Assets/Scripts/WorldScripts/InfGen.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfGen : MonoBehaviour
+{
+
+    public GameObject[] genChunks;
+    float chunkWidth;
+    float chunkLength;
+
+    Vector3 genPosition;
+    List<GameObject> chunkList = new List<GameObject>();
+
+
+    void OnTriggerEnter (Collider collider) {
+        if (collider.gameObject.CompareTag("Player")) {
+            chunkWidth = Random.Range(25, 40);
+            chunkLength = Random.Range(-2, 2);
+
+            genPosition = collider.transform.position + Vector3.right * chunkWidth + Vector3.up * chunkLength;
+
+            GameObject chunkItem = Instantiate(genChunks[Random.Range(0, genChunks.Length)], genPosition, Quaternion.identity);
+            chunkList.Add(chunkItem);
+
+            Debug.Log(chunkList.Count);
+        }
+        if (chunkList.Count > 5) {
+            Destroy(chunkList[0]);
+            chunkList.RemoveAt(0);
+
+            Debug.Log("chunk removed");
+        }
+    }
+}
